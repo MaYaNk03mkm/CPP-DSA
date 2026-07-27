b28732f63ded49b00302e155aa2431938d3b26c3
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-07-28 05:16:32 +0530

    variable done

---
+++ 01_Basics/B_HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    //1st code in cpp
+    cout << "Hello World" << endl;
+    cout << "My name is mayank";
+    return 0;
+}
---
+++ 01_Basics/B_Variable.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //whole number
+    int a = 10;//declare and assign
+    int year = 2026;
+
+    //double = decimal number
+    double price = 99.99;
+    
+    //character type
+    char grade = 'A';
+    
+    //boolean
+    bool isStudent = false;
+    
+    //String
+    string fname = "MAYANK";
+    string lname = "Sharma";
+    cout << fname <<" " << lname;
+    return 0;
+}
