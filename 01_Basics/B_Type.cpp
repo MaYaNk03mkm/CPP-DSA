abbed4aad34a4db4de319f31bad4b363273056ee
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-07-28 21:45:41 +0530

    using typedef and using

--- 01_Basics/B_Type.cpp
+++ 01_Basics/B_Type.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-typedef int num;
-typedef std::string text;
-typedef bool bo;
+// typedef int num;
+// typedef std::string text;
+// typedef bool bo;
 
+
+// by using 
+using num = int;
+using text = std::string;
+using bo = bool; 
 int main()
 {
     //typedef is short form of "int a = 5" we can also write typedef int num and use in code.
@@ -18,6 +23,7 @@ int main()
     //this is bool
     bo isStudent = true;
     std::cout<<isStudent;
-
+    // we can also done this by using USING in strting of the code in above the int main//
+    // in thi swe comment the typedef and use the using and variable and then we and datatype of the variable and that done.
     return 0;
 }
