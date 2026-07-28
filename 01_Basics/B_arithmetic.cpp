caa90424c495975d687eaffb229f74a793e26422
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-07-29 05:03:07 +0530

    arithmetic op done

---
+++ 01_Basics/B_arithmetic.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main()
+{
+    //Arithmetic operator
+    // int student = 10;
+    // student = student + 1;
+    // student++;
+    // student+=1;
+
+    // // Subtract
+    // student = student - 1;
+    // student--;
+    // student-=1;
+
+    //multiplication
+    // student = student * 2;
+    // student * 2;
+    
+    //division
+    // student = student / 2;
+    // student/=2;
+
+    //Modulus(remainder)
+    // int remain = student % 3;
+    // std::cout<<student;
+    // std::cout<<remain;
+
+    // cpp is sequence to solve the some problem in some order
+    // 1st parenthesis and then multiplication and division and then addition and subtract
+    int fo = 6-5+4*3/2; // answerr should be 7 1st it solve the multiplication and then divsion and then add and subtract
+    std::cout<<fo;
+    return 0;
+}
