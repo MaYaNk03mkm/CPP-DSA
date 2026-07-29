d2293bef3093aaaf8d28b2c2c30154d712ad7b11
Mayank Sharma <mayanksharma.feb03@gmail.com>
2026-07-29 05:36:02 +0530

    type conversion

---
+++ 01_Basics/B_typeconv.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    double pi = (int)3.14;
+    // std::cout<<pi;
+    // char x = 100;
+    // char x = (char)100; 
+    std::cout<<(char)999;
+    return 0;
+}
